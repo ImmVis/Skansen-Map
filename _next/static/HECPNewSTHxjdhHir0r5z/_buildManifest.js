547bc54d0cac52ed9a044f15a7c34c339577cc84
Golen87 <Golen87@users.noreply.github.com>
2023-05-16 12:33:43 +0000

    deploy: a590a9b76f094a2333a6b5ab16daa2b980a55b44

--- _next/static/HECPNewSTHxjdhHir0r5z/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/7d0bf13e-483b1888049c24d8.js","static/css/1ee2da1e6e147d16.css","static/chunks/pages/index-b47464cec1d9811d.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
